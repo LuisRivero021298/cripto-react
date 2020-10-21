67a42dc7aefe522935aad55a82c1338f3300018d
LuisRivero021298 <luisda021298@gmail.com>
2020-10-20 19:21:51 -0500

    Create TableItem style (molecule)

--- src/components/CoinItem.js
+++ src/components/CoinItem.js
@@ -3,9 +3,11 @@ import React from "react";
 function CoinItem({ c }) {
   return (
     <React.Fragment>
-      <tr>
-        <td></td>
-        <td>{c.id}</td>
+      <tr className="Table__item">
+        <td>
+          <div className="simule_img"></div>
+        </td>
+        <td>{c.rank}</td>
         <td>{c.symbol}</td>
         <td>{c.priceUsd}</td>
       </tr>
