5a493e6bd1ead68c39f97a4d187b5cc575ef62ab
LuisRivero021298 <luisda021298@gmail.com>
2020-11-18 20:02:42 -0500

    build whit webpack

---
+++ webpack.config.js
@@ -0,0 +1,91 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+const AddAssetHtmlPlugin = require('add-asset-html-webpack-plugin');
+const TerserJSPlugin = require("terser-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const publicPath = 'http://localhost:5500/dist/';
+
+module.exports = {
+  entry: {
+    app: path.resolve(__dirname, "src/index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/[name].[hash].js",
+    publicPath,
+    chunkFilename: 'js/[id].[chunkhash].js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist"),
+    hot: true,
+    port: 9000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js|jsx$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          "css-loader",
+          "sass-loader"
+        ],
+      },
+      {
+        test: /\.jpg|png|gif|jpeg|woff|eot|ttf|svg|mp4|webm$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 1000,
+            name: '[hash].[ext]',
+            outputPath: 'assets'
+          },
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      new TerserJSPlugin(), 
+      new OptimizeCSSAssetsPlugin()
+    ],
+    splitChunks: {
+      chunks: 'all',
+      minSize: 0,
+      name: 'commos'
+    }
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[hash].css',
+      chunkFilename: 'css/[id].[hash].css'
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public/index.html")
+    }),
+    new webpack.DllReferencePlugin({
+      manifest: require('./modules-manifest.json')
+    }),
+    new AddAssetHtmlPlugin({
+      filepath: path.resolve(__dirname, "dist/js/*.dll.js"),
+      outputPath: 'js',
+      publicPath: `${publicPath}js`
+    }),
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ["**/app.**", "**/modules.**"]
+    })
+  ],
+   resolve: {
+    extensions: [".js", ".jsx"]
+  }
+};
